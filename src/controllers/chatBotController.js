ae102cd075aa188653d5539f3eaf1544306b4177
SirriCelles <sirricelles@gmail.com>
2021-02-03 08:24:05 +0100

    Natural Language Processing added

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -66,52 +66,52 @@ let getWebhook = (req, res) => {
 };
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
-    let response;
+// function handleMessage(sender_psid, received_message) {
+//     let response;
 
-  // Check if the message contains text
-  if (received_message.text) {    
+//   // Check if the message contains text
+//   if (received_message.text) {    
 
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  else if (received_message.attachments) {
+//     // Create the payload for a basic text message
+//     response = {
+//       "text": `You sent the message: "${received_message.text}". Now send me an image!`
+//     }
+//   }  else if (received_message.attachments) {
   
-    // Gets the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Yes!",
-                  "payload": "yes",
-                },
-                {
-                  "type": "postback",
-                  "title": "No!",
-                  "payload": "no",
-                }
-              ],
-            }]
-          }
-        }
-      }
+//     // Gets the URL of the message attachment
+//     let attachment_url = received_message.attachments[0].payload.url;
+//     response = {
+//         "attachment": {
+//           "type": "template",
+//           "payload": {
+//             "template_type": "generic",
+//             "elements": [{
+//               "title": "Is this the right picture?",
+//               "subtitle": "Tap a button to answer.",
+//               "image_url": attachment_url,
+//               "buttons": [
+//                 {
+//                   "type": "postback",
+//                   "title": "Yes!",
+//                   "payload": "yes",
+//                 },
+//                 {
+//                   "type": "postback",
+//                   "title": "No!",
+//                   "payload": "no",
+//                 }
+//               ],
+//             }]
+//           }
+//         }
+//       }
   
-  }  
+//   }  
   
-  // Sends the response message
-  callSendAPI(sender_psid, response);
+//   // Sends the response message
+//   callSendAPI(sender_psid, response);
 
-}
+// }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
@@ -143,7 +143,7 @@ function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v6.0/me/messages",
+    "uri": "https://graph.facebook.com/v9.0/me/messages",
     "qs": { "access_token": process.env.DEVC_CHATBOT_PAGE_TOKEN },
     "method": "POST",
     "json": request_body
@@ -156,6 +156,49 @@ function callSendAPI(sender_psid, response) {
     }
   }); 
   
+}
+
+
+function firstTrait(nlp, name) {
+    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
+}
+
+function handleMessage(sender_psid, message) {
+    //handle message for react, like press like button
+    // id like button: sticker_id 369239263222822
+
+    if( message && message.attachments && message.attachments[0].payload){
+        callSendAPI(sender_psid, "Thank you for watching my video !!!");
+        callSendAPIWithTemplate(sender_psid);
+        return;
+    }
+
+    let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
+    let entityChosen = "";
+    entitiesArr.forEach((name) => {
+        let entity = firstTrait(message.nlp, name);
+        if (entity && entity.confidence > 0.8) {
+            entityChosen = name;
+        }
+    });
+
+    if(entityChosen === ""){
+        //default
+        callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
+    }else{
+       if(entityChosen === "wit$greetings"){
+           //send greetings message
+           callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
+       }
+       if(entityChosen === "wit$thanks"){
+           //send thanks message
+           callSendAPI(sender_psid,`You 're welcome!`);
+       }
+        if(entityChosen === "wit$bye"){
+            //send bye message
+            callSendAPI(sender_psid,'bye-bye!');
+        }
+    }
 }
 
 module.exports = {
