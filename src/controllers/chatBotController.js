583ab6395bd109bc9460a9ac543d7c4df5542ec8
SirriCelles <sirricelles@gmail.com>
2021-02-07 14:01:37 +0100

    webhook 500 error

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -33,9 +33,7 @@ let postWebhook = (req, res) =>{
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
-            if (typeof webhook_event.message === 'undefined') {
-              callSendAPI(sender_psid, "Hello I am Deve! Nice to see you! How may I assist? - Please select the START button to Start a conversation")
-            }else if (webhook_event.message) {
+            if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
